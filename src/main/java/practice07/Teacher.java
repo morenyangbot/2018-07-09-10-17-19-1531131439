e48de8994a3b2aebe64c77d9bec012d40e2ba956
Teng YANG <teng.yang@oocl.com>
2019-07-11 19:23:19 +0800

    Implement practice 7

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,43 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String string = super.introduce() + " I am a Teacher.";
+        if (getKlass() != null) {
+            string += " I teach " + getKlass().getDisplayName() + ".";
+        } else {
+            string += " I teach No Class.";
+        }
+        return string;
+    }
+
+    public String introduceWith(Student student) {
+        String string = super.introduce() + " I am a Teacher.";
+        if (getKlass() != null && student.getKlass().getNumber() == getKlass().getNumber()) {
+            string += " I teach " + student.getName() + ".";
+        } else {
+            string += " I don't teach " + student.getName() + ".";
+        }
+        return string;
+    }
 }
