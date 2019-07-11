20389aea7455c39fdb7942a91c04a4f7b36f5d54
Teng YANG <teng.yang@oocl.com>
2019-07-11 19:14:57 +0800

    Implement practice 6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,33 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass = -1;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String string = super.introduce() + " I am a Teacher.";
+        if (getKlass() != -1) {
+            string += " I teach Class " + getKlass() + ".";
+        } else {
+            string += " I teach No Class.";
+        }
+        return string;
+    }
 }
